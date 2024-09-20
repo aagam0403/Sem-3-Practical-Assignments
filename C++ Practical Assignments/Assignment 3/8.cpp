43be89a465b81e0f04a5ba653a217b673ebe199e
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2024-09-20 18:44:21 +0530

    Add files via upload

---
+++ C++ Practical Assignments/Assignment 3/8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int countElements(int arr[], int size) {
+    return size;
+}
+
+int main() {
+    int n;
+    
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    
+    int arr[n];
+    
+    cout << "Enter the elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    cout << "Number of elements in the array: " << countElements(arr, n) << endl;
+    
+    return 0;
+}
