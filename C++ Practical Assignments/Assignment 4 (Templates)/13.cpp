20847574132e4448fa415200937570de3fb3bb98
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2024-09-21 12:43:27 +0530

    Add files via upload

---
+++ C++ Practical Assignments/Assignment 4 (Templates)/13.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    string s1;
+    cout<<"Enter String = ";
+    cin>>s1;
+    cout<<"The string is = "<<s1<<endl;
+    cout<<"This string length = "<<s1.length()<<endl;
+    return 0;
+}
