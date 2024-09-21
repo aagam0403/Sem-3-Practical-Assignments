3ce52b335966fced7193f9c87721dc6476a94433
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2024-09-21 12:22:05 +0530

    Add files via upload

---
+++ C++ Practical Assignments/Assignment 3/14.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    string s1;
+    cout<<"Enter String 1 = ";
+    cin>>s1;
+    cout<<"The string is = "<<s1<<endl;
+    cout<<"This string capacity = "<<s1.capacity()<<endl;
+    return 0;
+}
