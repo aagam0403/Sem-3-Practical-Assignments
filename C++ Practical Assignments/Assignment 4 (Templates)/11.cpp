554ceb3bba1d9d831d05179c564d64578493724f
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2024-10-06 15:57:28 +0530

    Update 11.cpp

--- C++ Practical Assignments/Assignment 4 (Templates)/11.cpp
+++ C++ Practical Assignments/Assignment 4 (Templates)/11.cpp
@@ -1,40 +1,24 @@
 #include <iostream>
 using namespace std;
-class Student 
+template<class T>
+void decToBinary(T n)
 {
-public:
-    int roll;
-    string name;
-    void get() 
-    {
-        cout << "Enter Roll Number and Name: ";
-        cin >> roll;
-        getline(cin, name);
-    }
-    void display() 
-    {
-        cout << "Roll Number: " << roll << endl;
-        cout << "Name: " << name << endl;
-    }
-};
+    int binaryNum[32];
 
-int main() 
+    int i = 0;
+    while (n > 0) {
+
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+
+    for (int j = i - 1; j >= 0; j--)
+        cout << binaryNum[j];
+}
+
+int main()
 {
-    Student students[10];
-
-    for (int i = 0; i < 10; i++) 
-    {
-        cout << "Enter details for student " << i + 1 << ":" << endl;
-        students[i].get();
-        cout << endl;
-    }
-
-    cout << "Displaying student details:" << endl;
-    for (int i = 0; i < 10; i++) 
-    {
-        cout << "Details of student " << i + 1 << ":" << endl;
-        students[i].display();
-        cout << "-------------------------" << endl;
-    }
+    decToBinary(10);
     return 0;
 }
