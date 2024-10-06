4acc89eade2861e3e3fdb2a79c1b23440e42f205
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2024-10-06 15:50:45 +0530

    Update 2.cpp

--- C++ Practical Assignments/Assignment 4 (Templates)/2.cpp
+++ C++ Practical Assignments/Assignment 4 (Templates)/2.cpp
@@ -1,26 +1,12 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+template<class T>
+T add(T a,T b)
+{
+	return (a+b);
+}
+
 int main()
 {
-    int i,j;
-    cout<<"Enter the no. of rows & cols : ";
-    cin>>i>>j;
-    int a[i][j];
-    for(int x=0;x<i;x++)
-    {
-        for(int y=0;y<j;y++)
-        {
-            cout<<"Enter the element ["<<x<<"]["<<y<<"]";
-            cin>>a[x][y];   
-        }
-    }
-    for(int x=0;x<i;x++)
-    {
-        for(int y=0;y<j;y++)
-        {
-            cout<<"\t"<<a[x][y];   
-        }
-        cout<<"\n";
-    }
-    return 0;
+	cout<<"Addition : "<<add(7,5);
 }
