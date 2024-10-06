aef57eaca97ea196a5a698d8bc9bd2d12f0f0188
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2024-10-06 15:58:11 +0530

    Update 13.cpp

--- C++ Practical Assignments/Assignment 4 (Templates)/13.cpp
+++ C++ Practical Assignments/Assignment 4 (Templates)/13.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
-int main() 
+template<class T>
+void ar(T a)
 {
-    string s1;
-    cout<<"Enter String = ";
-    cin>>s1;
-    cout<<"The string is = "<<s1<<endl;
-    cout<<"This string length = "<<s1.length()<<endl;
-    return 0;
+	cout<<"Area of square : "<<(a*a)<<endl;
+}
+template<class T1,class T2>
+void ar(T1 a,T2 b)
+{
+	cout<<"Area of Rectangle : "<<(a*b)<<endl;
+}
+int main()
+{
+	ar(5);
+	ar(3,7);
 }
