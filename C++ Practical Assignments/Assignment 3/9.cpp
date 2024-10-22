1edcee34d57b0efda5c06fccbc258c719c991209
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2024-10-22 09:41:52 +0530

    Update 9.cpp

--- C++ Practical Assignments/Assignment 3/9.cpp
+++ C++ Practical Assignments/Assignment 3/9.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 using namespace std;
-
-int sumArray(int arr[], int size) {
+int sumArray(int arr[], int size) 
+{
     int sum = 0;
-    for(int i = 0; i < size; i++) {
+    for(int i = 0; i < size; i++) 
+    {
         sum += arr[i];
     }
     return sum;
 }
-
-int main() {
+int main() 
+{
     int n;
-
     cout << "Enter the number of elements: ";
     cin >> n;
-
     int arr[n];
-
     cout << "Enter the elements: ";
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) 
+    {
         cin >> arr[i];
     }
-
     int total = sumArray(arr, n);
-    
     cout << "Total of all elements: " << total << endl;
-
     return 0;
 }
