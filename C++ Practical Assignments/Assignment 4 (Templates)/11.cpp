80e2b89e451064d6b28c3a8ecf281de23b0f24d5
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2024-10-22 09:50:29 +0530

    Update 11.cpp

--- C++ Practical Assignments/Assignment 4 (Templates)/11.cpp
+++ C++ Practical Assignments/Assignment 4 (Templates)/11.cpp
@@ -4,19 +4,16 @@ template<class T>
 void decToBinary(T n)
 {
     int binaryNum[32];
-
     int i = 0;
-    while (n > 0) {
-
+    while (n > 0) 
+    {
         binaryNum[i] = n % 2;
         n = n / 2;
         i++;
     }
-
     for (int j = i - 1; j >= 0; j--)
         cout << binaryNum[j];
 }
-
 int main()
 {
     decToBinary(10);
