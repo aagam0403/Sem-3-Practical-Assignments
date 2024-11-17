6158274f008b44340aa6c7f05fc541581cded868
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2024-11-17 15:31:04 +0530

    Update 8.cpp

--- C++ Practical Assignments/Assignment 3/8.cpp
+++ C++ Practical Assignments/Assignment 3/8.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
-
-int countElements(int arr[], int size) {
+int countElements(int arr[], int size) 
+{
     return size;
 }
-
-int main() {
-    int n;
-    
+int main() 
+{
+    int n;    
     cout << "Enter the number of elements: ";
     cin >> n;
-    
     int arr[n];
-    
     cout << "Enter the elements: ";
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) 
+    {
         cin >> arr[i];
     }
-    
     cout << "Number of elements in the array: " << countElements(arr, n) << endl;
-    
     return 0;
 }
