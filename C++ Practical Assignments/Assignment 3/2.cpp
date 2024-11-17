6256b87d9362c5fd3e5f03fab03e3224bcafcc80
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2024-11-17 15:09:45 +0530

    Update 2.cpp

--- C++ Practical Assignments/Assignment 3/2.cpp
+++ C++ Practical Assignments/Assignment 3/2.cpp
@@ -1,24 +1,27 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
-    int i,j;
-    cout<<"Enter the no. of rows & cols : ";
-    cin>>i>>j;
-    int a[i][j];
-    for(int x=0;x<i;x++)
+    int r,c;
+    cout<<"Enter Rows = ";
+    cin>>r;
+    cout<<"Enter Column = ";
+    cin>>c;
+    int a[r][c];
+    for(int i=0;i<r;i++)
     {
-        for(int y=0;y<j;y++)
+        for(int j=0;j<c;j++)
         {
-            cout<<"Enter the element ["<<x<<"]["<<y<<"]";
-            cin>>a[x][y];   
+            cout<<"Enter the Value of an array ["<<i<<"]["<<j<<"]";
+            cin>>a[i][j];
         }
     }
-    for(int x=0;x<i;x++)
+    
+    for(int i=0;i<r;i++)
     {
-        for(int y=0;y<j;y++)
+        for(int j=0;j<c;j++)
         {
-            cout<<"\t"<<a[x][y];   
+            cout<<a[i][j];
         }
         cout<<"\n";
     }
