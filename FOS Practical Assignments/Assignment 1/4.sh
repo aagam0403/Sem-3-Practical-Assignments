4728370958ae52aeda2622ffb6fe455c1b8899bf
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2024-11-21 21:31:15 +0530

    Update 4.sh

--- FOS Practical Assignments/Assignment 1/4.sh
+++ FOS Practical Assignments/Assignment 1/4.sh
@@ -1,13 +1,13 @@
 echo "Enter Basic Salary = "
 read basic
 
-DA=$(echo "scale=2;" $basic*60/100|bc)
-HRA=$(echo "scale=2;" $basic*20/100|bc)
+DA=$(echo "scale=2;" $basic*0.60 | bc)
+HRA=$(echo "scale=2;" $basic*0.20 | bc)
 MA=100
-IT=$(echo "scale=2;" $basic*15/100|bc)
-Gross=$(echo "scale=2;" $DA+$HRA+$MA+$IT|bc)
+IT=$(echo "scale=2;" $basic*0.15 | bc)
+GROSS=$(echo "scale=2;" $basic+$DA+$HRA+$MA+$IT | bc)
 echo "DA = $DA"
 echo "HRA = $HRA"
 echo "MA = $MA"
 echo "IT = $IT"
-echo "Gross Salary = $Gross"
+echo "GROSS = $GROSS"
